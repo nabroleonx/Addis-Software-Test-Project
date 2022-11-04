869695ef51e715667344ff98cfdc7ad66435e7ea
nabroleonx <nabroleonx@gmail.com>
2022-11-04 14:29:51 +0300

    Update mongo uri for production

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/addis", {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
